2e230a7e800cfccfbf9eb90e7c2753ed75d34254
Ruslan Romanenko <ruslan.roman19@mail.ru>
2021-06-25 05:39:04 +0300

    Начинаем программировать

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Main from '../main/main.jsx';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({title, genre, year}) {
+  return (
+    <Main title={title} genre={genre} year={year} />
+  );
 }
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import {getRandomInRange, getRandomItem} from './utils.js';
+import {TITLESFILMS, GENRES} from './const.js';
+
+function getFilmCard() {
+  return {
+    title: getRandomItem(TITLESFILMS),
+    genre: getRandomItem(GENRES),
+    year: getRandomInRange(2021, 1990),
+  };
+}
+
+const filmCard = getFilmCard();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      title={filmCard.title}
+      genre={filmCard.genre}
+      year={filmCard.year}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
