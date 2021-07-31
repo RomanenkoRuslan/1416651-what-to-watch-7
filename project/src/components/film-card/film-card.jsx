75792203d7cbe96b3964b2484287940e722ffbde
Ruslan Romanenko <ruslan.roman19@mail.ru>
2021-07-31 11:55:25 +0300

    Больше подробностей (часть 1)

--- project/src/components/film-card/film-card.jsx
+++ project/src/components/film-card/film-card.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Link} from 'react-router-dom';
+import VideoPlayer from '../video-player/video-player.jsx';
 
 function FilmCard(props) {
   const film = props.film;
-  // eslint-disable
-  // console.log(film);
+  const [isActive, setIsActive] = React.useState(false);
+  const timer = React.useRef(0);
+  const SECOND = 1000;
+
   return (
-    <article className="small-film-card catalog__films-card">
+    <article className="small-film-card catalog__films-card"
+      onMouseEnter={() => {
+        timer.current = setTimeout(() => setIsActive(true), SECOND);
+      }}
+
+      onMouseLeave={() => {
+        clearTimeout(timer.current);
+        setIsActive(false);
+      }}
+    >
       <div className="small-film-card__image">
-        <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={film.title} width="280" height="175" />
+        {!isActive && <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={film.title} width="280" height="175" />}
+        {isActive && <VideoPlayer src={film.video}/>}
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${film.id}`}>{film.title}</Link>
